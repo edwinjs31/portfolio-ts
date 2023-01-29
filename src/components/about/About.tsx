e7356422a66bb21a3cc919159477e5d1bc14d01c
edwinjs <edwinjs31@gmail.com>
2023-01-29 15:15:43 +0100

    Implement about component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
-import './about.css'
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import './about.css';
+import ME from '../../assets/me-about.jpg';
 export const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id='about'>
+      <h5>Get To know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img
+              src={ME}
+              alt='About Image'
+            />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Proyects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis quam ac ante accumsan, sollicitudin
+            iaculis ante fermentum. Aenean placerat vestibulum lacus quis efficitur. Donec volutpat sem sodales dolor
+            pretium, ac bibendum lectus efficitur.
+          </p>
+          <a
+            href='#contact'
+            className='btn btn-primary'
+          >
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};

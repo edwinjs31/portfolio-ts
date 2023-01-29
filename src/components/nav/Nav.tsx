cd9576efe7b140c41352a54ac154d1cf6a4ef3bc
edwinjs <edwinjs31@gmail.com>
2023-01-29 14:07:47 +0100

    implement nav component

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import './nav.css'
-export const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BsJournalBookmark } from 'react-icons/bs';
+import { RiServiceLine } from 'react-icons/ri';
+import { BiMessageSquareDetail } from 'react-icons/bi';
+import './nav.css';
+import { MouseEventHandler, useState } from 'react';
 
+export const Nav = () => {
+  const defaultActiveNav = '#';
+  const [activeNav, seTactiveNav] = useState(defaultActiveNav);
+
+  return (
+    <nav>
+      <a
+        href='#'
+        onClick={() => seTactiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href='#about'
+        onClick={() => seTactiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href='#experience'
+        className={activeNav === '#experience' ? 'active' : ''}
+        onClick={() => seTactiveNav('#experience')}
+      >
+        <BsJournalBookmark />
+      </a>
+      <a
+        href='#services'
+        className={activeNav === '#services' ? 'active' : ''}
+        onClick={() => seTactiveNav('#services')}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href='#contact'
+        className={activeNav === '#contact' ? 'active' : ''}
+        onClick={() => seTactiveNav('#contact')}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
+};

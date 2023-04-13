2883c2fe6e2008602b90e7c12c7b2c24322bcfd0
edwinjs <edwinjs31@gmail.com>
2023-04-13 18:55:46 +0200

    add portfolio component

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import './portfolio.css'
+import { dataPortfolio } from '../../utils/api'
+
 export const Portfolio = () => {
+  
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {
+          dataPortfolio.map(({id, image, title, github, demo}) =>{
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt="" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
---
+++ src/utils/api.ts
@@ -0,0 +1,51 @@
+import IMG1 from '../assets/portfolio1.jpg';
+import IMG2 from '../assets/portfolio2.jpg';
+import IMG3 from '../assets/portfolio3.jpg';
+import IMG4 from '../assets/portfolio4.jpg';
+import IMG5 from '../assets/portfolio5.png';
+import IMG6 from '../assets/portfolio6.jpg';
+
+export const dataPortfolio = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'This is a portfolio item title 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'This is a portfolio item title 2',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'This is a portfolio item title 3',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'This is a portfolio item title 4',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'This is a portfolio item title 5',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'This is a portfolio item title 6',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/search/web-site',
+  },
+];

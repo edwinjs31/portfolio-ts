2c732e0a2e927d50005f5d6b4fc7c9ce1e0f0c42
edwinjs <edwinjs31@gmail.com>
2023-04-14 14:37:00 +0200

    add footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import {BsInstagram, BsFacebook, BsLinkedin} from 'react-icons/bs'
 import './footer.css'
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>EDWIN</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+        <a href="https://es-es.facebook.com/"><BsFacebook/></a>
+        <a href="https://www.instagram.com/"><BsInstagram/></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; EDWIN developer. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
